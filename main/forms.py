87f16e22fdd671e73f32247968319842c3c03bfc
donnobani <55165390+donnobani@users.noreply.github.com>
2025-04-13 13:13:28 -0400

    added create sheet template, view, and navbar button functionality

--- main/forms.py
+++ main/forms.py
@@ -2,10 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
-# UserRegistrationForm is a form that allows users to register for an account
 class UserRegistrationForm(UserCreationForm):
-    # UserCreationForm already has username and password fields
-    # Add a custom field for email
     email = forms.EmailField(
         required=True,
         widget=forms.EmailInput(attrs={
@@ -15,22 +12,23 @@ class UserRegistrationForm(UserCreationForm):
     )
 
     class Meta:
-        # this tells django which model to use
+        # Specifying the model to use
         model = User    
-        # this tells django which fields to include in the form
+        # Specifying the fields to include in the form
         fields = ('username', 'email', 'password1', 'password2') 
 
-    # customizing the form to use bootstrap classes
+    # Customizing the form to use bootstrap classes
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Add Bootstrap classes to all fields
+        # Add Bootstrap form-control class to all fields
         for field_name in self.fields:
             self.fields[field_name].widget.attrs['class'] = 'form-control'
 
-# UserLoginForm is a form that allows users to login to their account
+
 class UserLoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Add Bootstrap form-control class to username and password fields
         self.fields['username'].widget.attrs.update({
             'class': 'form-control',
             'placeholder': 'Enter your username'
--- main/views.py
+++ main/views.py
@@ -8,6 +8,7 @@ def home(request):
     return render(request, "home.html")
 
 def login(request):
+    # If POST request, process form data
     if request.method == 'POST':
         form = UserLoginForm(request, data=request.POST)
         if form.is_valid():
@@ -18,12 +19,13 @@ def login(request):
                 auth_login(request, user)
                 return redirect('home')
     else:
+        # If GET request, create empty form
         form = UserLoginForm()
     
     return render(request, "login.html", {'form': form})
 
-
 def signup(request):
+    # If POST request, process form data
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
@@ -33,12 +35,17 @@ def signup(request):
             messages.success(request, 'Registration successful!')
             return redirect('home')
     else:
+        # If GET request, create empty form
         form = UserRegistrationForm()
     
     return render(request, "signup.html", {'form': form})
 
 def logout(request):
+
     if request.method == 'POST':
         auth_logout(request)
         messages.success(request, 'You have been logged out successfully.')
     return redirect('home')
+
+def createsheet(request):
+    return render(request, "createsheet.html")
